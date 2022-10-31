82cf92fb93089d1c2cc324d56aaea3c8eb5a6346
Taavi <taavi247@gmail.com>
2022-10-31 10:43:19 +0200

    Deleted unused app

--- reactdataexplorer/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReactdataexplorerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reactdataexplorer'
--- reactdataexplorer/javascript/DataExplorer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DataExplorer = () => {
-    return (
-        <div>React! Under construction.</div>
-    )
-}
-
-export default DataExplorer
